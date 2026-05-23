82e1b8fa2098d43ada2d4ef311018579ebf51ec4
Rafaela Campos <rafacamposs@gmail.com>
2026-05-23 19:46:59 -0300

    feat: melhora fluxo de usuários, pets, agendamentos e notificações

--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,20 @@
+import axios from "axios";
+
+const api = axios.create({
+ baseURL: "https://api.netcao.com.br/api",
+  withCredentials: true
+ });
+
+ export default api;
+
+
 // import axios from "axios";
 
 // const api = axios.create({
-//   baseURL: "https://api.netcao.com.br/api",
+//   baseURL: "/api",
 //   withCredentials: true
 // });
 
 // export default api;
-
-
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "/api",
-  withCredentials: true
-});
-
-export default api;
 
 //alterar quando for para produção, para o endereço do backend, e retirar o proxy do package.json
--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-
+//https://vite.dev/config/
+ 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api.netcao.com.br',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
-})
+ })
+
+
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://api.netcao.com.br',
+//         changeOrigin: true,
+//         secure: false,
+//       }
+//     }
+//   }
+// })
 
 //alterar quando for para produção, para o endereço do backend, e retirar o proxy do package.json
