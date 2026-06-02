a212c8fa30a921eaf3893d34f6b47983bac5a1ec
GuilhermeFaag <guilhermefag2@outlook.com>
2026-06-02 18:24:21 -0300

    chore: ajusta rota da api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://api.netcao.com.br/api",
   withCredentials: true
 });
 export default api;
