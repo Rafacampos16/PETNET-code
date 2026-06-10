d05f31e03d2be9efc1be8aaf6d7696ed1a40bf85
Gabriel Aziz <gabriel.silvaabdelaziz@gmail.com>
2026-06-10 10:33:12 -0300

    correção do nome da variável de ambiente

--- src/services/apiLogs.js
+++ src/services/apiLogs.js
@@ -1,23 +1,12 @@
 import axios from "axios";
- 
+
 const apiLogs = axios.create({
-  baseURL: import.meta.env.API_LOGS_BASE_URL,
+  baseURL: import.meta.env.VITE_API_LOGS_BASE_URL,
   headers: {
-    Authorization: import.meta.env.API_LOGS_TOKEN,
+    Authorization: import.meta.env.VITE_API_LOGS_TOKEN,
   },
 
 });
- 
-export default apiLogs;
- 
-// import axios from "axios";
-// const apiLogs = axios.create({
-//   baseURL: "/api-logs",
-//     headers: {
-//    Authorization: "seu-token-estatico-aqui",
- // },
 
-// });
-// export default apiLogs;
-// alterar quando for para produção, para o endereço do backend de logs, e configurar o proxy no package.json
- 
+export default apiLogs;
+
